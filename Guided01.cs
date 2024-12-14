5f00b1780db69fe389f0a4243cd72a9688991a84
Carlos barbosa <carloslk18@gmail.com>
2024-12-14 01:32:19 -0300

    choread: just class Guided01

--- Guided01.cs
+++ Guided01.cs
@@ -7,30 +7,100 @@ public class Guided01{
 
     public static void StudentScores(){
 
-        int currentAssignments = 4;
+        int examAssignments = 5;
 
-        int [] sophiaScore = {90, 86, 87, 98};
-        int [] andrewScore = {92, 89, 81, 96};
-        int [] emmaScore = {90, 85, 87, 98};
-        int [] loganScore = {90, 95, 87, 88};
-        
-        int sophiaSum;
-        int sandrewSum; 
-        int emmaSum;
-        int loganSum;
+        int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+        int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+        int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+        int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
 
-        double sofiaSocore;
-        double andrewSocore;
-        double emmaSocore;
-        double loganSocore;
+        string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 
+        int[] studentScores = new int[10];
 
-       string [] students = {"Sophia", "Andrew", "Emma", "Logan"};
+        string currentStudentLetterGrade = "";
 
-       foreach (string names in students){
+        Console.WriteLine("Student\t\tGrade\n");
 
-            Console.WriteLine(names);
-       }
+        foreach (string name in studentNames)
+        {
+            string currentStudent = name;
+
+            if (currentStudent == "Sophia")
+            studentScores = sophiaScores;
+
+            else if (currentStudent == "Andrew")
+                studentScores = andrewScores;
+
+            else if (currentStudent == "Emma")
+                studentScores = emmaScores;
+
+            else if (currentStudent == "Logan")
+                studentScores = loganScores;
+
+        int sumAssignmentScores = 0;
+
+        decimal currentStudentGrade = 0;
+
+        int gradedAssignments = 0;
+
+            foreach (int score in studentScores)
+            {
+                gradedAssignments += 1;
+
+                if (gradedAssignments <= examAssignments)
+                    sumAssignmentScores += score;
+
+                else
+                    sumAssignmentScores += score / 10;
+            }
+
+            currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
+
+            if (currentStudentGrade >= 97)
+                currentStudentLetterGrade = "A+";
+
+            else if (currentStudentGrade >= 93)
+                currentStudentLetterGrade = "A";
+
+            else if (currentStudentGrade >= 90)
+                currentStudentLetterGrade = "A-";
+
+            else if (currentStudentGrade >= 87)
+                currentStudentLetterGrade = "B+";
+
+            else if (currentStudentGrade >= 83)
+                currentStudentLetterGrade = "B";
+
+            else if (currentStudentGrade >= 80)
+                currentStudentLetterGrade = "B-";
+
+            else if (currentStudentGrade >= 77)
+                currentStudentLetterGrade = "C+";
+
+            else if (currentStudentGrade >= 73)
+                currentStudentLetterGrade = "C";
+
+            else if (currentStudentGrade >= 70)
+                currentStudentLetterGrade = "C-";
+
+            else if (currentStudentGrade >= 67)
+                currentStudentLetterGrade = "D+";
+
+            else if (currentStudentGrade >= 63)
+                currentStudentLetterGrade = "D";
+
+            else if (currentStudentGrade >= 60)
+                currentStudentLetterGrade = "D-";
+
+            else
+                currentStudentLetterGrade = "F";
+
+            Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+        }
+
+            Console.WriteLine("\n\rPress the Enter key to continue");
+            Console.ReadLine();
     }
 }
 }
